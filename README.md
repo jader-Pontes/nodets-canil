9c4e43e1aee15bc17b3c593583f53364f4c48130
jader <jaderluiz01@gmail.com>
2022-08-04 15:22:55 -0300

    Primeiro commit controllers criado

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # nodets-canil
+Projeto feito no curso de Node na escola b7web.
+ Node + Typescript.
+
+### Pré-requisitos globais:
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para rodar o projeto
+`npm run start-dev`
+
---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRoutes from './routes/index';
+
+
+//config servidor
+dotenv.config();
+
+const server = express();
+
+server.set('view engine', 'mustache');
+server.set('view', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+//Rotas
+server.use(mainRoutes);
+
+server.use((req, res) => {
+    res.send('página não encontrada!');
+});
+
+
+
+
+
+//Servidor Porta
+server.listen(process.env.PORT);
+
+
+
+
