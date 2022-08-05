f4c773d34c5fc9ad8ee43c0e71f6171ec6be4132
jader <jaderluiz01@gmail.com>
2022-08-05 09:10:29 -0300

    Projeto finalizado projeto feito com Template Enginner

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const server = express();
 
 server.set('view engine', 'mustache');
-server.set('view', path.join(__dirname, 'views'));
+server.set('views', path.join(__dirname, './views'));
 server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')));
@@ -20,7 +20,8 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes);
 
 server.use((req, res) => {
-    res.send('página não encontrada!');
+
+    res.render('pages/404');
 });
 
 
